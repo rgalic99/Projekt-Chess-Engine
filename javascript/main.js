8625c2fc3fe913b02251e1757fa02a65a40c9605
Roko Galić <roko.gali7@gmail.com>
2021-03-22 13:43:07 +0100

    Tidied up main file

--- javascript/main.js
+++ javascript/main.js
@@ -5,30 +5,33 @@ $(function () {
 const init = () => {
 	console.log("Inicijalizacija...");
 	InitBoard();
-	Init_Board120to64();
 };
 
 const InitBoard = () => {
-	FilesBoard.fill(SQUARES.OFFBOARD);
-	RanksBoard.fill(SQUARES.OFFBOARD);
-
-	for (let rank = RANKS.RANK_1; rank <= RANKS.RANK_8; rank++) {
-		for (let file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
-			let square = FileRankToSquare(file, rank);
-			FilesBoard[square] = file;
-			RanksBoard[square] = rank;
-		}
-	}
+	InitBoardArrays();
+	PopulateBoardArrays();
 };
 
-const Init_Board120to64 = () => {
+const InitBoardArrays = () => {
 	Board_120_to_64.fill(-1);
 	Board_64_to_120.fill(-1);
-	let square64 = 0;
+	FilesBoard.fill(SQUARES.OFFBOARD);
+	RanksBoard.fill(SQUARES.OFFBOARD);
+};
 
-	for (let rank = RANKS.RANK_1; rank <= RANKS.RANK_8; rank++) {
-		for (let file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
-			let square = FileRankToSquare(file, rank);
+const PopulateBoardArrays = () => {
+	let square64 = 0;
+	let square = 0;
+	let rank = 0;
+	let file = 0;
+
+	for (rank = RANKS.RANK_1; rank <= RANKS.RANK_8; rank++) {
+		for (file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
+			square = FileRankToSquare(file, rank);
+
+			FilesBoard[square] = file; // populate FilesBoard
+			RanksBoard[square] = rank; // populate RanksBoard
+
 			Board_64_to_120[square64] = square;
 			Board_120_to_64[square] = square64;
 			square64++;
