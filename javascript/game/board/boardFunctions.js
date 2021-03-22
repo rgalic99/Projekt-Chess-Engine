c69eb1373be896a3e165117b53e4e3b6dc53d9d0
Roko Galić <roko.gali7@gmail.com>
2021-03-22 15:55:42 +0100

    Added addPiece lambda and comments

--- javascript/game/board/boardFunctions.js
+++ javascript/game/board/boardFunctions.js
@@ -6,37 +6,19 @@ const GetSquare120 = (square64) => {
 	return Board_64_to_120[square64]; //vraća indeks kocke zadan preko formule 21 + file + rank * 10
 };
 
-const ResetBoard = () => {
-	GameBoard.pieces.fill(SQUARES.OFFBOARD);
-	GameBoard.pieceList.fill(PIECES.EMPTY);
-	GameBoard.material.fill(0);
-	GameBoard.pieceNum.fill(0);
-	for (let index = 0; index < 64; index++)
-		GameBoard.pieces[GetSquare120(index)] = PIECES.EMPTY;
-
-	GameBoard.side = COLORS.BOTH;
-	GameBoard.enPassant = SQUARES.NO_SQ;
-	GameBoard.fiftyMoveRule = 0;
-	GameBoard.ply = 0;
-	GameBoard.historyPly = 0;
-	GameBoard.castlePerm = 0;
-	GameBoard.posKey = 0;
-	GameBoard.moveListStart[GameBoard.ply] = 0;
-};
-
 // Primjer FEN-a:	rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
 const parseFEN = (FENstring) => {
 	ResetBoard();
 	let fenCount = 0; //index of the character in FENstring
 
 	fenCount = parsePieces(FENstring, fenCount);
-	if (fenCount == -1) return;
+	if (fenCount == -1) throw new Error("FEN is invalid"); //if FEN is invalid
 
 	fenCount = parseSide(FENstring, fenCount);
 	fenCount = parseCastle(FENstring, fenCount);
 	fenCount = parseEnPassant(FENstring, fenCount);
 
-	GameBoard.posKey = GeneratePositionKey();
+	GameBoard.posKey = GeneratePositionKey(); // generate position hash
 };
 
 const parsePieces = (FENstring, fenCount) => {
@@ -45,6 +27,7 @@ const parsePieces = (FENstring, fenCount) => {
 
 	while (rank >= RANKS.RANK_1 && fenCount < FENstring.length) {
 		let count = 1;
+		//čita se svaki znak i stavlja na tu kocku tu figuru
 		switch (FENstring[fenCount]) {
 			case "p":
 				piece = PIECES.bP;
@@ -92,34 +75,37 @@ const parsePieces = (FENstring, fenCount) => {
 			case "7":
 			case "8":
 				piece = PIECES.EMPTY;
-				count = Number(FENstring[fenCount]);
+				count = Number(FENstring[fenCount]); //ako nije figura dodjeli se broj praznih kocki
 				break;
 
 			case "/":
 			case " ":
-				rank--;
-				file = FILES.FILE_A;
-				fenCount++;
+				rank--; //prelazak u idući red
+				file = FILES.FILE_A; //vraća na prvu kocku
+				fenCount++; //pomak za 1 karakter
 				continue;
 			default:
 				console.log("FEN error");
 				return -1;
 		}
-
-		for (let i = 0; i < count; i++) {
-			let square120 = FileRankToSquare(file, rank);
-			GameBoard.pieces[square120] = piece;
-			file++;
-		}
-		fenCount++;
+		addPiece(count, FileRankToSquare(file, rank), piece); //dodavanje figure na kocku ili dodavanje count praznih kocki
+		file += count; //pomak za 1 ili broj praznih polja
+		fenCount++; //pomak za 1 karakter
 	}
 
 	return fenCount;
 };
 
+const addPiece = (count, square, piece) => {
+	for (let i = 0; i < count; i++) {
+		GameBoard.pieces[square] = piece; //dodavanje figure ili više praznih kocaka
+		square++;
+	}
+};
+
 const parseSide = (FENstring, fenCount) => {
-	GameBoard.side(FENstring[fenCount] == "w") ? COLORS.WHITE : COLORS.BLACK; //parse whose turn it is
-	fenCount += 2; //move 2 characters forward
+	GameBoard.side(FENstring[fenCount] == "w") ? COLORS.WHITE : COLORS.BLACK; //parsiraj čiji je red
+	fenCount += 2; //pomak za 2 karaktera
 	return fenCount;
 };
 
@@ -127,7 +113,7 @@ const parseCastle = (FENstring, fenCount) => {
 	while (FENstring[fenCount] != " ") {
 		switch (FENstring[fenCount]) {
 			case "K":
-				GameBoard.castlePerm |= CASTLEBIT.WKCA;
+				GameBoard.castlePerm |= CASTLEBIT.WKCA; //operacija ILI nad castlePerm vrijednosti
 				break;
 			case "Q":
 				GameBoard.castlePerm |= CASTLEBIT.WQCA;
@@ -141,9 +127,9 @@ const parseCastle = (FENstring, fenCount) => {
 			default:
 				break;
 		}
-		fenCount++;
+		fenCount++; //pomak za 1 karakter
 	}
-	fenCount++;
+	fenCount++; //pomak za 1 karakter
 
 	return fenCount;
 };
@@ -151,9 +137,10 @@ const parseCastle = (FENstring, fenCount) => {
 const parseEnPassant = (FENstring, fenCount) => {
 	if (FENstring[fenCount] != "-") {
 		file = FENstring[fenCount].charCodeAt() - "a".charCodeAt();
-		rank = Number(FENstring[fenCount + 1]) - 1;
-		GameBoard.enPassant = FileRankToSquare(file, rank);
+		rank = FENstring[fenCount + 1].charCodeAt() - "1".charCodeAt();
+		GameBoard.enPassant = FileRankToSquare(file, rank); //postavljanje indeksa en passant kocke
 	}
+	fenCount++; //pomak za 1 karakter
 
 	return fenCount;
 };
@@ -161,19 +148,36 @@ const parseEnPassant = (FENstring, fenCount) => {
 const GeneratePositionKey = () => {
 	let PositionKey = 0;
 
-	for (let square = 0; square < NUM_OF_SQ; square++) {}
-	{
-		let piece = GameBoard.pieces[square];
-		if (piece && piece != SQUARES.OFFBOARD)
-			PositionKey ^= PieceKeys[piece * 120 + square];
+	for (let square = 0; square < 64; square++) {
+		let piece = GameBoard.pieces[GetSquare120(square)];
+		if (piece != PIECES.EMPTY)
+			PositionKey ^= PieceKeys[piece * 120 + square]; //operacija XOR nad PositionKey vrijednosti za svaku figuru
 	}
 
-	if (GameBoard.side == COLORS.WHITE) PositionKey ^= SideKey;
+	if (GameBoard.side == COLORS.WHITE) PositionKey ^= SideKey; //XOR sa stranom koja igra
 
 	if (GameBoard.enPassant != SQUARES.NO_SQ)
-		PositionKey ^= PieceKeys[GameBoard.enPassant];
+		PositionKey ^= PieceKeys[GameBoard.enPassant]; //XOR sa en passant kockom
 
-	PositionKey ^= CastleKeys[GameBoard.castlePerm];
+	PositionKey ^= CastleKeys[GameBoard.castlePerm]; //XOR sa pravima rokade
 
 	return PositionKey;
+};
+
+const ResetBoard = () => {
+	GameBoard.pieces.fill(SQUARES.OFFBOARD);
+	GameBoard.pieceList.fill(PIECES.EMPTY);
+	GameBoard.material.fill(0);
+	GameBoard.pieceNum.fill(0);
+	for (let index = 0; index < 64; index++)
+		GameBoard.pieces[GetSquare120(index)] = PIECES.EMPTY;
+
+	GameBoard.side = COLORS.BOTH;
+	GameBoard.enPassant = SQUARES.NO_SQ;
+	GameBoard.fiftyMoveRule = 0;
+	GameBoard.ply = 0;
+	GameBoard.historyPly = 0;
+	GameBoard.castlePerm = 0;
+	GameBoard.posKey = 0;
+	GameBoard.moveListStart[GameBoard.ply] = 0;
 };
