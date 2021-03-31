5679a810a8345ccff8e4b55d75fe8462c6b61ee0
Roko Galić <roko.gali7@gmail.com>
2021-03-31 16:10:29 +0200

    Rewrote every function that checks attacks so that they work properly

--- javascript/game/moveGen.js
+++ javascript/game/moveGen.js
@@ -1,81 +1,93 @@
 const SquareAttacked = (square, side) => {
-	if (AttackedByPawn(square, side)) return Boolean.True;
-	if (AttackedByKnight(square, side)) return Boolean.True;
-	if (AttackedByBishopOrQueen(square, side)) return Boolean.True;
-	if (AttackedByRookOrQueen(square, side)) return Boolean.True;
-	if (AttackedByKing(square, side)) return Boolean.True;
-	return Boolean.False;
+	if (AttackedByPawn(square, side)) return Bool.True;
+	if (AttackedByKnight(square, side)) return Bool.True;
+	if (AttackedByRookOrQueen(square, side)) return Bool.True;
+	if (AttackedByBishopOrQueen(square, side)) return Bool.True;
+	if (AttackedByKing(square, side)) return Bool.True;
+	return Bool.False;
 };
 
 const AttackedByPawn = (square, side) => {
 	//white=0, black=1
 	if (
-		GameBoard.pieces[square + 22 * side - 11] == side //if side is white then -11 otherwise +11
-			? PIECES.bP //if black is playing side will be 1 and therefore true so we check black pawns
-			: PIECES.wP || GameBoard.pieces[square + 18 * side - 9] == side //if side is white then -9 otherwise +9
-			? PIECES.bP
-			: PIECES.wP //if white is playing side will be 0 and therefore false so we check white pawns
+		GameBoard.pieces[square + 22 * side - 11] == //if side is white then square-11 otherwise square+11
+			(side ? PIECES.bP : PIECES.wP) || //if black is playing side will be 1 and therefore true so we check black pawns
+		GameBoard.pieces[square + 18 * side - 9] == //if side is white then -9 otherwise +9
+			(side ? PIECES.bP : PIECES.wP) //if white is playing side will be 0 and therefore false so we check white pawns
 	)
-		return Boolean.True;
-	return Boolean.False;
+		return Bool.True;
+	return Bool.False;
 };
-
 const AttackedByKnight = (square, side) => {
-	knightDirection.forEach((knightSquare) => {
-		let piece = GameBoard.pieces[square + knightSquare];
-		if (
-			piece != SQUARES.OFFBOARD &&
-			pieceCol[piece] == side &&
-			pieceKnight[piece]
-		) {
-			return Boolean.True;
-		}
-	});
-	return Boolean.False;
+	return CheckKnightAndKing(square, side, knightDirection, pieceKnight);
+};
+const AttackedByKing = (square, side) => {
+	return CheckKnightAndKing(square, side, kingDirection, pieceKing);
 };
 
-const AttackedByKing = (square, side) => {
-	kingDirection.forEach((kingSquare) => {
-		let piece = GameBoard.pieces[square + kingSquare];
-		if (
-			piece != SQUARES.OFFBOARD &&
-			pieceCol[piece] == side &&
-			pieceKing[piece]
-		) {
-			return Boolean.True;
+const CheckKnightAndKing = (square, side, directionArray, pieceArray) => {
+	for (let index = 0; index < directionArray.length; index++) {
+		let piece = GameBoard.pieces[square + directionArray[index]];
+		if (CheckPiece(piece, side, pieceArray)) {
+			return Bool.True;
 		}
-	});
-	return Boolean.False;
+	}
+	return Bool.False;
 };
 
 const AttackedByBishopOrQueen = (square, side) => {
-	bishopDirection.forEach((direction) => {
-		do {
-			let current_square = square + direction;
-			let piece = GameBoard.pieces[current_square];
-			if (
-				piece != PIECES.EMPTY &&
-				pieceBishopQueen[piece] &&
-				pieceCol[piece] == side
-			)
-				return Boolean.True;
-		} while (piece != SQUARES.OFFBOARD);
-	});
-	return Boolean.False;
+	return CheckBishopAndRookAndQueen(
+		square,
+		side,
+		bishopDirection,
+		pieceBishopQueen
+	);
 };
 
 const AttackedByRookOrQueen = (square, side) => {
-	rookDirection.forEach((direction) => {
-		do {
-			let current_square = square + direction;
-			let piece = GameBoard.pieces[current_square];
-			if (
-				piece != PIECES.EMPTY &&
-				pieceRookQueen[piece] &&
-				pieceCol[piece] == side
-			)
-				return Boolean.True;
-		} while (piece != SQUARES.OFFBOARD);
-	});
-	return Boolean.False;
+	return CheckBishopAndRookAndQueen(
+		square,
+		side,
+		rookDirection,
+		pieceRookQueen
+	);
+};
+
+const CheckBishopAndRookAndQueen = (
+	square,
+	side,
+	directionArray,
+	pieceArray
+) => {
+	for (let index = 0; index < directionArray.length; index++) {
+		let current_square = square + directionArray[index];
+		let piece = GameBoard.pieces[current_square];
+
+		while (piece != SQUARES.OFFBOARD) {
+			if (CheckPiece(piece, side, pieceArray)) return Bool.True;
+
+			current_square += directionArray[index];
+			piece = GameBoard.pieces[current_square];
+		}
+	}
+	return Bool.False;
+};
+
+const CheckPiece = (piece, side, pieceArray) => {
+	return (
+		piece != PIECES.EMPTY && pieceArray[piece] && pieceCol[piece] == side
+	);
+};
+
+const PrintAttackedSquares = () => {
+	console.log("\nAttacked:\n");
+	for (let rank = RANKS.RANK_8; rank >= RANKS.RANK_1; rank--) {
+		let line = `${rank + 1}  `;
+		for (let file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
+			let square = FileRankToSquare(file, rank);
+			let piece = SquareAttacked(square, GameBoard.side) ? "X" : "-";
+			line += ` ${piece} `;
+		}
+		console.log(line);
+	}
 };
