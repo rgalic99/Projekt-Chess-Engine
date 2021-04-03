5b68f84ba2c644c07d4ab8fc7deaadab2accec2c
Roko Galić <roko.gali7@gmail.com>
2021-04-03 20:50:59 +0200

    Removed unecessary passing of the side parameter and changed it to color

--- javascript/game/squareAttacked.js
+++ javascript/game/squareAttacked.js
@@ -1,64 +1,65 @@
-const SquareAttacked = (square, side) => {
-	if (AttackedByPawn(square, side)) return Bool.True;
-	if (AttackedByKnight(square, side)) return Bool.True;
-	if (AttackedByRookOrQueen(square, side)) return Bool.True;
-	if (AttackedByBishopOrQueen(square, side)) return Bool.True;
-	if (AttackedByKing(square, side)) return Bool.True;
+const SquareAttacked = (square) => {
+	if (AttackedByPawn(square)) return Bool.True;
+	if (AttackedByKnight(square)) return Bool.True;
+	if (AttackedByRookOrQueen(square)) return Bool.True;
+	if (AttackedByBishopOrQueen(square)) return Bool.True;
+	if (AttackedByKing(square)) return Bool.True;
 	return Bool.False;
 };
 
 const AttackedByPawn = (square) => {
 	//white=0, black=1
+	const color = GameBoard.side;
 	if (
-		GameBoard.pieces[SquareOffset(square, side, 11)] ==
-			(side ? PIECES.wP : PIECES.bP) || //if black is playing we check black pawns
+		GameBoard.pieces[SquareOffset(square, 11)] ==
+			(color ? PIECES.wP : PIECES.bP) || //if black is playing we check black pawns
 		GameBoard.pieces[SquareOffset(square, 9)] ==
-			(side ? PIECES.wP : PIECES.bP) //if white is playing we check white pawns
+			(color ? PIECES.wP : PIECES.bP) //if white is playing we check white pawns
 	)
 		return Bool.True;
 	return Bool.False;
 };
-const AttackedByKnight = (square, side) => {
-	return CheckKnightAndKing(square, side, knightDirection, pieceKnight);
+const AttackedByKnight = (square) => {
+	return CheckKnightAndKing(square, knightDirection, pieceKnight);
 };
-const AttackedByKing = (square, side) => {
-	return CheckKnightAndKing(square, side, kingDirection, pieceKing);
-};
-
-const AttackedByBishopOrQueen = (square, side) => {
-	return CheckBishopAndRook(square, side, bishopDirection, pieceBishopQueen);
+const AttackedByKing = (square) => {
+	return CheckKnightAndKing(square, kingDirection, pieceKing);
 };
 
-const AttackedByRookOrQueen = (square, side) => {
-	return CheckBishopAndRook(square, side, rookDirection, pieceRookQueen);
+const AttackedByBishopOrQueen = (square) => {
+	return CheckBishopAndRook(square, bishopDirection, pieceBishopQueen);
 };
 
-const CheckKnightAndKing = (square, side, directionArray, pieceArray) => {
+const AttackedByRookOrQueen = (square) => {
+	return CheckBishopAndRook(square, rookDirection, pieceRookQueen);
+};
+
+const CheckKnightAndKing = (square, directionArray, pieceArray) => {
 	for (let index = 0; index < directionArray.length; index++) {
 		let piece = GameBoard.pieces[square + directionArray[index]];
-		if (CheckPiece(piece, side, pieceArray)) {
+		if (CheckPiece(piece, pieceArray)) {
 			return Bool.True;
 		}
 	}
 	return Bool.False;
 };
 
-const CheckPiece = (piece, side, pieceArray) => {
+const CheckPiece = (piece, pieceArray) => {
 	return (
 		piece != SQUARES.OFFBOARD &&
 		pieceArray[piece] &&
-		pieceCol[piece] == side
+		pieceCol[piece] == GameBoard.side
 	);
 };
 
-const CheckBishopAndRook = (square, side, directionArray, pieceArray) => {
+const CheckBishopAndRook = (square, directionArray, pieceArray) => {
 	for (let index = 0; index < directionArray.length; index++) {
 		let target_square = square + directionArray[index];
 		let piece = GameBoard.pieces[target_square];
 
 		while (piece != SQUARES.OFFBOARD) {
 			if (piece != PIECES.EMPTY) {
-				if (CheckPiece(piece, side, pieceArray)) return Bool.True;
+				if (CheckPiece(piece, pieceArray)) return Bool.True;
 				break;
 			}
 
@@ -75,7 +76,7 @@ const PrintAttackedSquares = () => {
 		let line = `${rank + 1}  `;
 		for (let file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
 			let square = FileRankToSquare(file, rank);
-			let piece = SquareAttacked(square, GameBoard.side) ? "X" : "-";
+			let piece = SquareAttacked(square) ? "X" : "-";
 			line += ` ${piece} `;
 		}
 		console.log(line);
