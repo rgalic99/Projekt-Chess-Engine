93919fa3dc417e6b5e504814dd50532b57c4a588
Roko Galić <roko.gali7@gmail.com>
2021-06-13 13:15:09 +0200

    Added TakeMove function

---
+++ javascript/game/takeMove.js
@@ -0,0 +1,57 @@
+const TakeMove = () => {
+	GameBoard.historyPly--;
+	GameBoard.ply--;
+
+	const move = GameBoard.history[GameBoard.historyPly].move;
+	const from = fromSquare(move);
+	const to = toSquare(move);
+
+	if (GameBoard.enPassant != SQUARES.NO_SQ) HashEnPassant();
+	HashCastle();
+
+	GameBoard.castlePerm = GameBoard.history[GameBoard.historyPly].castlePerm;
+	GameBoard.fiftyMoveRule =
+		GameBoard.history[GameBoard.historyPly].fiftyMoveRule;
+	GameBoard.enPassant = GameBoard.history[GameBoard.historyPly].enPassant;
+
+	if (GameBoard.enPassant != SQUARES.NO_SQ) HashEnPassant();
+	HashCastle();
+
+	GameBoard.side ^= 1;
+	HashSide();
+
+	const color = GameBoard.side;
+	if (move & moveFlagEnPassant)
+		AddPiece(SquareOffset(to, -10), color ? PIECES.bP : PIECES.wP);
+	else if (move & moveFlagCastle)
+		switch (to) {
+			case SQUARES.C1:
+				MovePiece(SQUARES.D1, SQUARES.A1);
+				break;
+			case SQUARES.C8:
+				MovePiece(SQUARES.D8, SQUARES.A8);
+				break;
+			case SQUARES.G1:
+				MovePiece(SQUARES.F1, SQUARES.H1);
+				break;
+			case SQUARES.G8:
+				MovePiece(SQUARES.F8, SQUARES.H8);
+				break;
+			default:
+				break;
+		}
+
+	MovePiece(to, from);
+
+	const captured = capturedPiece(move);
+	if (captured != PIECES.EMPTY) AddPiece(to, captured);
+	const promoted = promotedPiece(move);
+
+	if (promoted != PIECES.EMPTY) {
+		ClearPiece(from);
+		AddPiece(
+			from,
+			pieceCol[promoted] === COLORS.WHITE ? PIECES.wP : PIECES.bP
+		);
+	}
+};
--- javascript/utilities/dependencies.js
+++ javascript/utilities/dependencies.js
@@ -11,6 +11,7 @@ const GetDependencies = () => {
 	game/squareAttacked.js
 	game/moveGen.js
 	game/makeMove.js
+	game/takeMove.js
 	game/main.js`;
 	return links.split("\n");
 };
