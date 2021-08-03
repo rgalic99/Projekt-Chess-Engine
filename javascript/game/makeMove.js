852c29d339efe5652b2c2df852870d5dc659ac12
Roko Galić <roko.gali7@gmail.com>
2021-08-03 17:34:33 +0200

    Fixed the MakeMove checking the wrong side when it comes to check

--- javascript/game/makeMove.js
+++ javascript/game/makeMove.js
@@ -8,11 +8,10 @@ const ClearPiece = (square) => {
 
 	const index = GameBoard.pieceList.indexOf(square, PieceIndex(piece, 0));
 	const last = --GameBoard.pieceNum[piece];
-	GameBoard.pieceList[PieceIndex(piece, index)] =
-		GameBoard.pieceList[PieceIndex(piece, last)]; // na indeks figure postavlja se indeks zadnje figure tog tipa
+	GameBoard.pieceList[index] = GameBoard.pieceList[PieceIndex(piece, last)]; // na indeks figure postavlja se indeks zadnje figure tog tipa
 };
 
-const AddPiece = (piece, square) => {
+const AddPiece = (square, piece) => {
 	HashPiece(piece, square); // figura se hashira u novu kocku
 
 	const color = pieceCol[piece];
@@ -34,9 +33,80 @@ const MovePiece = (from, to) => {
 	GameBoard.pieceList[index] = to; // pamti se kocka na kojoj se figura nalazi
 };
 
+const TakeMove = () => {
+	// vraćanje unazad
+	GameBoard.historyPly--;
+	GameBoard.ply--;
+
+	const move = GameBoard.history[GameBoard.historyPly].move;
+	const from = fromSquare(move);
+	const to = toSquare(move);
+
+	// ako je potez bio enPassant
+	if (GameBoard.enPassant != SQUARES.NO_SQ) HashEnPassant();
+
+	// hashiranje prava rokade
+	HashCastle();
+
+	// ažuriranje vrijednosti
+	GameBoard.castlePerm = GameBoard.history[GameBoard.historyPly].castlePerm;
+	GameBoard.fiftyMoveRule =
+		GameBoard.history[GameBoard.historyPly].fiftyMoveRule;
+	GameBoard.enPassant = GameBoard.history[GameBoard.historyPly].enPassant;
+
+	// ako je potez bio enPassant
+	if (GameBoard.enPassant != SQUARES.NO_SQ) HashEnPassant();
+
+	// hashiranje prava rokade
+	HashCastle();
+
+	// promjena strane
+	GameBoard.side ^= 1;
+	HashSide();
+
+	const color = GameBoard.side;
+	// ako je potez enPassant
+	if (move & moveFlagEnPassant)
+		AddPiece(SquareOffset(to, 10), color ? PIECES.wP : PIECES.bP);
+	// ako je potez rokada
+	else if (move & moveFlagCastle)
+		switch (to) {
+			case SQUARES.C1:
+				MovePiece(SQUARES.D1, SQUARES.A1);
+				break;
+			case SQUARES.C8:
+				MovePiece(SQUARES.D8, SQUARES.A8);
+				break;
+			case SQUARES.G1:
+				MovePiece(SQUARES.F1, SQUARES.H1);
+				break;
+			case SQUARES.G8:
+				MovePiece(SQUARES.F8, SQUARES.H8);
+				break;
+			default:
+				break;
+		}
+
+	MovePiece(to, from);
+
+	// vraćanje oduzete figure ako postoji
+	const captured = capturedPiece(move);
+	if (captured != PIECES.EMPTY) AddPiece(to, captured);
+
+	// vraćanje poteza u slučaju promocije ako se dogodi
+	const promoted = promotedPiece(move);
+
+	if (promoted != PIECES.EMPTY) {
+		ClearPiece(from);
+		AddPiece(from, pieceCol[promoted] ? PIECES.bP : PIECES.wP);
+	}
+};
+
 const MakeMove = (move) => {
 	const from = fromSquare(move);
 	const to = toSquare(move);
+	const side = GameBoard.side;
+	const edis = !side;
 
 	GameBoard.history[GameBoard.historyPly].posKey = GameBoard.posKey;
 	// ako je potez enPassant
@@ -111,10 +181,9 @@ const MakeMove = (move) => {
 	// promjena strane
 	GameBoard.side ^= 1;
 	HashSide();
-	const side = GameBoard.side;
 
 	// provjera je li potez moguć
-	if (SquareAttacked(GameBoard.pieceList[PieceIndex(kings[side], 0)], side)) {
+	if (SquareAttacked(GameBoard.pieceList[PieceIndex(kings[side], 0)], edis)) {
 		TakeMove();
 		return Bool.False;
 	}
